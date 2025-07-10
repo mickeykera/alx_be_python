f38b0766341d8332589a464d5826f77383304343
Michael <132381288+mickeykera@users.noreply.github.com>
2025-07-10 19:59:24 +0300

    Add files via upload

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,12 @@
+number1 = 10
+number2 = 5
+
+# Perform calculations
+sum = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+
+# Print results in the requested format
+print(f"Addition of {number1} and {number2} is {sum}")
+print(f"Subtraction of {number1} and {number2} is {subtraction}")
+print(f"Multiplication of {number1} and {number2} is {multiplication}")
