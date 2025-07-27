787166351e6c606468b977772ef12d207256c162
Mike <mickeykera7@gmail.com>
2025-07-27 14:36:42 +0300

    added explore_datetime

---
+++ fns_and_dsa/datetime_formatter.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+
+def display_current_datetime():
+    current_date = datetime.now()
+    formatted_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
+    print(f"Current Date and Time: {formatted_date}")
+
+if __name__ == "__main__":
+    display_current_datetime()
---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,15 @@
+from datetime import datetime, timedelta
+
+def display_current_datetime():
+    current_date = datetime.now()
+    formatted_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
+    print(f"Current Date and Time: {formatted_date}")
+
+def calculate_future_date(future_date):
+    days = int(future_date)
+    future_datetime = datetime.now() + timedelta(days=days)
+    return future_datetime.strftime("%Y-%m-%d %H:%M:%S")
+if __name__ == "__main__":
+    display_current_datetime()
+    future_date = input("Enter the number of days: ")
+    print(f"Future Date: {calculate_future_date(future_date)}")
