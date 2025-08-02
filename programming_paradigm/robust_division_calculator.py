838f562c2a016eb7e0045fb1d2d4f9225af089cd
Mike <mickeykera7@gmail.com>
2025-08-02 23:04:06 +0300

    Added new file

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,14 @@
+def safe_divide(numerator, denominator):
+    try:
+        numerator = float(numerator)
+    except ValueError:
+        print("Error: Invalid numerator")
+    try:
+        denominator = float(denominator)
+    except ValueError:
+        print("Error: Invalid denominator")
+    try:
+        return numerator / denominator
+    except ZeroDivisionError:
+        print("Error: Division by zero")
+    
